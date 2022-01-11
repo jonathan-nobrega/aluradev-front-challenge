3728c7c1047455c8785429450cbff134a4135f8d
Jonathan Silva <eng.jonathan.moraes@gmail.com>
2022-01-11 13:45:52 -0300

    css adaptions for tablet resolution

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,9 @@ export default function App() {
             <img src="img/Photo.png" alt='user' />
             <a style={{ "margin-left": "15px" }} href="localhost:3000">Jony</a>
           </div>
+          <div className="header-menu">
+            <img src="img/Menu.svg" alt='menu' />
+          </div>
         </div>
       </div><div className="page">
         <div className="menu">
@@ -35,10 +38,10 @@ export default function App() {
         <div className="codeSpace">
           <div className="codeBox">
             <div className="codeBox-editor">
-              <div className="codebox-editor-circles">
-                <div className="codeBox-editor-circleGreen"></div>
-                <div className="codeBox-editor-circleOrange"></div>
-                <div className="codeBox-editor-circleGreen"></div>
+              <div className="codebox-editor-allCircles">
+                <div className="codeBox-editor-circle" style={{ "background": "green" }}></div>
+                <div className="codeBox-editor-circle" style={{ "background": "orange" }}></div>
+                <div className="codeBox-editor-circle" style={{ "background": "green" }}></div>
               </div>
               <textarea className="codeBox-editor-input" placeholder="escreva seu codigo"></textarea>
             </div>
@@ -49,26 +52,27 @@ export default function App() {
         </div>
         <div className="codeInfo">
           <p className="menu-title">SEU PROJETO</p>
-          <input className="codeInfo-project" type="text" placeholder="Nome do seu projeto" />
-          <textarea className="codeInfo-project" type="text" rows="3" placeholder="Descrição do seu projeto"></textarea>
+          <input className="codeInfo-text" type="text" placeholder="Nome do seu projeto" />
+          <textarea className="codeInfo-text" type="text" rows="3" placeholder="Descrição do seu projeto"></textarea>
           <p className="menu-title">PERSONALIZAÇÃO</p>
-          <select className="codeInfo-project" id="language" name="Language">
-            <option value="javascript">JavaScript</option>
-            <option value="html">HTML</option>
-            <option value="css">CSS</option>
-          </select>
-          <select className="codeInfo-project" id="color" name="Color">
-            <option style={{ "background": "red" }} value="red">Vermelho</option>
-            <option style={{ "background": "blue" }} value="blue">Azul</option>
-            <option style={{ "background": "green" }} value="green">Verde</option>
-            <option style={{ "background": "yellow" }} value="yellow">Amarelo</option>
-          </select>
+          <div className="codeInfo-personalize">
+            <select className="codeInfo-dropdown" id="language" name="Language">
+              <option value="javascript">JavaScript</option>
+              <option value="html">HTML</option>
+              <option value="css">CSS</option>
+            </select>
+            <select className="codeInfo-dropdown" id="color" name="Color">
+              <option style={{ "background": "red" }} value="red">Vermelho</option>
+              <option style={{ "background": "blue" }} value="blue">Azul</option>
+              <option style={{ "background": "green" }} value="green">Verde</option>
+              <option style={{ "background": "yellow" }} value="yellow">Amarelo</option>
+            </select>
+          </div>
           <div className="button-saveProject">
             <a href="localhost:3000" style={{ "color": "black" }}>Salvar projeto</a>
           </div>
         </div>
       </div>
     </>
-
   );
 }
